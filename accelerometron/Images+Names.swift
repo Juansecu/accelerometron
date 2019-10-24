6f009037a6bb7d80d29fe429fe9ad5320f974477
Juan <juan_cuello@MacBook-Air-de-Juan.local>
2019-10-24 10:50:28 -0500

    Accelerometron is ended.

---
+++ accelerometron/Images+Names.swift
@@ -0,0 +1,9 @@
+//
+//  Images+Names.swift
+//  accelerometron
+//
+//  Created by Juan on 10/23/19.
+//  Copyright © 2019 Juan. All rights reserved.
+//
+
+import Foundation
---
+++ accelerometron/View/ParallaxCell.swift
@@ -0,0 +1,24 @@
+//
+//  ParallaxCell.swift
+//  accelerometron
+//
+//  Created by Juan on 10/23/19.
+//  Copyright © 2019 Juan. All rights reserved.
+//
+
+import UIKit
+
+class ParallaxCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
